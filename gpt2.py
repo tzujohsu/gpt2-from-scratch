a09343263d2e457bfcb1844085d7477a6e2515ea
Tzu-Jo Hsu <tzujohsu@umich.edu>
2025-07-02 22:37:32 -0400

    implement MLP and Block class

--- gpt2.py
+++ gpt2.py
@@ -3,6 +3,31 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 
+class MLP(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.c_fc   = nn.Linear(config.n_embd, 4 * config.n_embd) 
+        self.c_proj = nn.Linear(4 * config.n_embd, config.n_embd)
+    
+    def forward(self, x):
+        x = self.c_fc(x)
+        x = self.gelu(x) # an approximation version
+        x = self.c_proj(x)
+        return x
+
+class Block(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.ln_1 = nn.LayerNorm(config.n_embd)
+        self.attn = CausalSelfAttention(config)
+        self.ln_2 = nn.LayerNorm(config.n_embd)
+        self.mlp  = MLP(config)
+    
+    def forward(self, x):
+        x = x + self.attn(self.ln_1(x)) # exchange info and communicate, reduce operation
+        x = x + self.mlp(self.ln_2(x)) # map operation
+        return x
+
 @dataclass
 class GPTConfig:
     block_size: int = 245
@@ -18,10 +43,10 @@ class GPT(nn.Module):
         self.config = config
 
         self.transformer = nn.ModuleDict(dict(
-            wte = nn.Embedding(config.vocab_size, config.n_embd),
-            wpe = nn.Embedding(config.block_size, config.n_embd),
+            wte = nn.Embedding(config.vocab_size, config.n_embd), # word embedding table
+            wpe = nn.Embedding(config.block_size, config.n_embd), # position embedding table
             h = nn.ModuleList([Block(config) for _ in range(config.n_layer)]), # Block is a custom class to be defined
-            ln_f = nn.LayerNorm(config.n_embd),
+            ln_f = nn.LayerNorm(config.n_embd), # final layer norm
         ))
 
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
