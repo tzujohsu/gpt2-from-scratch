e75c75e8ea3b25a81f713ecf626d17f035422043
Tzu-Jo Hsu <tzujohsu@umich.edu>
2025-07-09 13:00:01 -0400

    implement DataLoaderLite class

--- gpt2.py
+++ gpt2.py
@@ -181,7 +181,39 @@ class GPT(nn.Module):
                     sd[k].copy_(sd_hf[k])
 
         return model
+
+# ------------------------------------------------------------
+import tiktoken
+
+class DataLoaderLite:
+    def __init__(self, B, T):
+        self.B = B
+        self.T = T
+
+        with open('input.txt', 'r') as f:
+            text = f.read()
+        enc = tiktoken.get_encoding("gpt2")
+        tokens = enc.encode(text)
+        self.tokens = torch.tensor(tokens)
+        print(f"Loaded {len(self.tokens)} tokens")
+        print(f"1 epoch = {len(self.tokens) // (B * T)} batches")
+
+        # state
+        self.current_position = 0
+
+    def next_batch(self):
+        B, T = self.B, self.T
+        buf = self.tokens[self.current_position : self.current_position+B*T+1]
+        x = (buf[:-1].view(B, T)) # inputs
+        y = (buf[1:].view(B, T)) # targets
+        # advance the position in the tensor (by B*T)
+        self.current_position += B*T
+        if self.current_position + (B * T +1) > len(self.tokens):
+            self.current_position = 0
+        
+        return x, y
     
+
 # ------------------------------------------------------------
 # attempt to autodetect the device
 device = 'cpu'
@@ -194,16 +226,7 @@ print(f"using device: {device}")
 # ------------------------------------------------------------
 # Initilalzing from scratch
 
-import tiktoken
-enc = tiktoken.get_encoding("gpt2")
-with open('input.txt', 'r') as f:
-    text = f.read()
-text = text[:1000]
-tokens = enc.encode(text)
-B, T = 4, 32
-buf = torch.tensor(tokens[:B*T + 1])
-x = buf[:-1].view(B, T)
-y = buf[1:].view(B, T)
+train_loader = DataLoaderLite(B=4, T=32)
 
 # get logits
 model = GPT(GPTConfig())
@@ -212,6 +235,9 @@ model.to(device)
 # optimize
 optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
 for i in range(50):
+    x, y = train_loader.next_batch()
+    x = x.to(device)
+    y = y.to(device)
     optimizer.zero_grad()
     logits, loss = model(x, y)
     loss.backward()
